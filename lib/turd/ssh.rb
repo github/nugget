96859dbc0817acd094fcb55570184330da2e5fdf
Matt Burke <spraints@gmail.com>
2017-03-22 14:39:57 -0400

    Cleaner implementation of the ssh check

--- lib/turd/ssh.rb
+++ lib/turd/ssh.rb
@@ -8,23 +8,32 @@ module Turd
       host = options.fetch(:server)
       username = options.fetch(:username)
       command = options.fetch(:command)
-      stdin = options.fetch(:stdin, "")
+      stdin = options.fetch(:stdin, nil)
       # todo - :keys => filenames or :key_data => PEM id data
       response = {:stdout => "", :stderr => "", :start => Time.now.to_f}
-      status = {}
       Net::SSH.start(host, username) do |ssh|
         response[:connected] = Time.now.to_f
-        last_time = Time.now.to_f
-        channel = ssh.exec command, status do |ch, stream, data|
-          last_time = Time.now.to_f
-          response[stream] << data
+        ssh.open_channel do |channel|
+          channel.exec(command) do |ch, success|
+            if success
+              channel.send_data(stdin) if stdin
+              channel.eof!
+              channel.on_request("exit-status") do |_, data|
+                response[:exit_status] = data.read_long
+              end
+              channel.on_data do |_, data|
+                response[:stdout] << data
+              end
+              channel.on_extended_data do |_, data|
+                response[:stderr] << data
+              end
+            end
+          end
         end
-        ssh.loop(0.5) { Time.now.to_f - last_time < 10.0 }
-        channel.send_data(stdin)
-        response[:last_out] = last_time
+        ssh.loop
       end
       response[:finished] = Time.now.to_f
-      response.merge(status)
+      response
     end
 
     def self.assert_response(request_definition, response, response_definition)
