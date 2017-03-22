9643816ea97c43e1640d8798723326e5621728d2
Matt Burke <spraints@gmail.com>
2017-03-22 18:53:06 -0400

    Add timing for ssh check

--- lib/turd/ssh.rb
+++ lib/turd/ssh.rb
@@ -10,32 +10,33 @@ module Turd
       command = options.fetch(:command)
       stdin = options.fetch(:stdin, nil)
       # todo - :keys => filenames or :key_data => PEM id data
-      response = {:stdout => "", :stderr => "", :start => Time.now.to_f}
+      response = {:stdout => "", :stderr => ""}
+      started = Time.now.to_f
+      connected = exited = stdout_started = nil
       Net::SSH.start(host, username) do |ssh|
-        response[:connected] = Time.now.to_f
+        connected = Time.now.to_f
         ssh.open_channel do |channel|
           channel.exec(command) do |ch, success|
             if success
               channel.send_data(stdin) if stdin
               channel.eof!
               channel.on_request("exit-status") do |_, data|
-                response[:exited] = Time.now.to_f
+                exited = Time.now.to_f
                 response[:exit_status] = data.read_long
               end
               channel.on_data do |_, data|
-                response[:first_stdout] ||= Time.now.to_f
+                stdout_started = Time.now.to_f
                 response[:stdout] << data
-              end
-              channel.on_extended_data do |_, data|
-                response[:first_stderr] ||= Time.now.to_f
-                response[:stderr] << data
               end
             end
           end
         end
         ssh.loop
       end
-      response[:finished] = Time.now.to_f
+      finished = Time.now.to_f
+      response[:total_time] = finished - started
+      response[:read_time] = exited - connected if exited and connected
+      response[:sshauth_time] = connected - started if connected
       response
     end
 
@@ -56,6 +57,7 @@ module Turd
           end
         end
       end
+      response
     end
   end
 
